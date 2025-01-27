25705bff253a302538cc59b3d54834effeb93ed1
SpaceExploring1 <40209752@yonder.nl>
2025-01-27 21:05:06 +0100

    slideshow

--- index.php
+++ index.php
@@ -3,24 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Slideshow Background</title>
     <style>
-        body{
-            background-image: ;
+        body {
+            margin: 0;
+            overflow: hidden;
+            height: 100vh;
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
             color: white;
+            font-family: 'Times New Roman', Times, serif;
+            transition: background-image 1s ease-in-out;
         }
-        .GoldenOlive{
+
+        .GoldenOlive {
             color: #c7a752;
             font-size: 30px;
-            font-family: 'Times New Roman', Times, serif;
             text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
         }
     </style>
 </head>
 <body>
     <h1 class="GoldenOlive">Hello world</h1>
-</body>
-<script>
+    
+    <script>
+        const images = [
+            "../img/idyllic-farm-landscape-stockcake.jpg",
+            "../img/dreamstime_xl_71214312-scaled.jpg",
+            "../img/istockphoto-1403665879-612x612.jpg"
+        ];
 
-</script>
+        let currentIndex = 0;
+
+        function changeBackground() {
+            document.body.style.backgroundImage = `url('${images[currentIndex]}')`;
+            currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
+        }
+
+        // Initial background setup
+        changeBackground();
+
+        // Change background every 5 seconds
+        setInterval(changeBackground, 5000);
+    </script>
+</body>
 </html>
