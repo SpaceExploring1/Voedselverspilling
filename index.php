b075c60ac8e4b4db56b3e51492c42f2f0820d93e
SpaceExploring1 <40209752@yonder.nl>
2025-01-27 14:42:26 +0100

    H1

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            background-image: ;
+            color: white;
+        }
+        .GoldenOlive{
+            color: #c7a752;
+            font-size: 30px;
+            font-family: 'Times New Roman', Times, serif;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hoi</h1>
+    <h1 class="GoldenOlive">Hello world</h1>
 </body>
 <script>
 
