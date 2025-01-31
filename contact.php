5e65d875e97fb86427a8d4401b00dce125297c01
SpaceExploring1 <40209752@yonder.nl>
2025-01-31 01:46:05 +0100

    done with the nav

--- contact.php
+++ contact.php
@@ -10,8 +10,22 @@
 <body>
 
 <header class="container-fluid bg-light d-flex justify-content-around align-items-center nav-text p-2">
-    <!-- Navigation links go here -->
-</header>
+        <a href="index.php">Home</a>
+        <a href="about.php">Over</a>
+        <a href="contact.php">Contact</a>
+        <a href="presentatie.php">Presentatie</a>
+        <a href="poster.php">Poster</a>
+        <a href="petitie.php">Petitie</a>
+        <a href="https://www.instagram.com/appelcatering/">
+            <img style="width:40px; height:40px;" src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram">
+        </a>
+        <a href="https://www.facebook.com/appelcatering"></a>
+            <img style="width:40px; height:40px;" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook">
+        </a>
+        <a href="https://www.linkedin.com/connect-services/ads_experience/?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fappel-bv%2F"></a>
+            <img style="width:40px; height:40px;" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn">
+        </a>
+    </header>
 
 <div class="form-container">
     <h2>Maak contact met ons!</h2>
